ba5db4ed0ad2bbd231cc59d800a8798848bfaca6
Nistal Jr Mario <Mario.Nistal@Emerson.com>
2024-11-25 14:23:30 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using GetCursorState;
+
+namespace TestCursor
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Press any key to exit...");
+            while (!Console.KeyAvailable)
+            {
+                GetCursorState();
+                Thread.Sleep(200); // 2000 milliseconds = 2 seconds
+            }
+        }
+
+        private static void GetCursorState()
+        {
+            GetCursorStateMethods.CheckCursorType();
+        }
+    }
+}
